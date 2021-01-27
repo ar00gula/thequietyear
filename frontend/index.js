382e217a669fc31422f3eff3a7d438a1defbc1c1
ar00gula <arianna.mayer@gmail.com>
2021-01-27 14:48:03 -0500

    pencil, marker, eraser, color picker all working, but cannot get eraser to say eraser if I click a color during it

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,5 @@
+//toggle sizes
+
 let sideBar = document.querySelectorAll(".sidebar")
 
 function changeSize(item, size) {
@@ -18,10 +20,68 @@ function changeSize(item, size) {
 
 sideBar.forEach(item => {
   item.addEventListener("click", function(e) {
-    changeSize(item, "smol");
+    if (e.target.classList.contains("sidebar")) {
+      changeSize(item, "smol");
+    }
   })
 })
-    
+
+
+//brushes
+function brushColor(color) {
+  ctx.strokeStyle = `rgb(${color})`
+}
+
+function brushWeight(weight) {
+  ctx.lineWidth = weight
+}
+
+function colorRecall() {
+    brushColor(rememberedColor)
+  }
+
+let rememberedColor = "0, 0, 0"
+document.getElementById("pencil").addEventListener("click", e => {
+  brushWeight(1);
+  colorRecall()
+})
+  
+document.getElementById("marker").addEventListener("click", e => {
+  brushWeight(4);
+  colorRecall()
+})
+
+document.getElementById("eraser").addEventListener("click", e => {
+  brushColor("255, 232, 189")
+  brushWeight(8)
+})
+
+let colorButton = document.getElementById("colorDropdown")
+colorButton.addEventListener("click", e => {
+  if (e.target.id === "color") {
+    if (document.getElementById("colorList").classList.contains("hidden")){
+      document.getElementById("colorList").classList.remove("hidden")
+    } else {
+      document.getElementById("colorList").classList.add("hidden")
+    }
+  }
+})
+document.getElementById("red").addEventListener("click", e => {
+  if (ctx.strokeStyle === 'rgb(255, 232, 189)') {
+    brushColor("255, 232, 189")
+  } else {
+    brushColor("144, 0, 0");
+    rememberedColor = "144, 0, 0";
+  }
+})
+document.getElementById("black").addEventListener("click", e => {
+  brushColor("0, 0, 0");
+  rememberedColor = "0, 0, 0";
+})
+
+
+
+
 //Map
 
   let map = document.getElementById('map')
@@ -38,16 +98,7 @@ sideBar.forEach(item => {
   map.addEventListener('mousedown', startDraw, false);
   map.addEventListener('mousemove', draw, false);
   map.addEventListener('mouseup', endDraw, false);
-
-  // map.addEventListener('click', function(e) {
-  //     console.log(`x ${e.clientX}`)
-  //     console.log(`y ${e.clientY}`)
-  // })
-
-
-  function marker() {
-    ctx.lineWidth = 4
-  }
+  
   function draw(e) {
     if(!isActive) return;
 
@@ -61,7 +112,6 @@ sideBar.forEach(item => {
 
   function drawOnCanvas(plots) {
       ctx.beginPath();
-      console.log(plots)
       ctx.moveTo(plots[0].x, plots[0].y);
     
       for(let i=1; i<plots.length; i++) {
