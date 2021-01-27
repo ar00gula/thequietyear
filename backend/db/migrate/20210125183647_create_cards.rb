d07824188bbe55deea0b5c24d7cb0adbe14eeab3
ar00gula <arianna.mayer@gmail.com>
2021-01-27 18:34:52 -0500

    building card class, adding card images

--- backend/db/migrate/20210125183647_create_cards.rb
+++ backend/db/migrate/20210125183647_create_cards.rb
@@ -1,6 +1,7 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
+      t.string :name
       t.string :suite
       t.string :value
       t.string :description
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ace_of_spring = Card.create(name: "Ace of Spring", suite: "diamonds", value: "1", description:"What group has the highest status in the community? What must people do to gain inclusion in this group?
+    <br>
+    or...
+    <br>
+    Are there distinct family units in the community? If so, what family structures are common?")
