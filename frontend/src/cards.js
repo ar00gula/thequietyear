78a25baa9b031b71db11fe65c675d58238d4161c
ar00gula <arianna.mayer@gmail.com>
2021-01-28 11:02:17 -0500

    working on card flip animation

--- frontend/src/cards.js
+++ frontend/src/cards.js
@@ -1,20 +1,44 @@
 let drawCard = document.getElementById("draw-card")
-let drawCardBack = document.getElementById("flip-card-back")
+let flipCardInner = document.getElementById("flip-card-inner")
+let flipCardFront = document.getElementById("flip-card-front")
+let flipCardBack = document.getElementById("flip-card-back")
+// let drawCardBack = document.getElementById("flip-card-back")
 
 drawCard.addEventListener("click", function() {
     flipCard()
+    document.getElementById("discard-pile").src = "images/kingofspring.png"
+
 })
 
-drawCardBack.addEventListener("click", function() {
-    flipCardBack()
-})
+// drawCard.addEventListener("mouseup", function() {
+//     flipCardInner.classList.remove("transform")
+// })
+
+// drawCard.addEventListener("mouseup", function() {
+//     // document.getElementById("flip-card-inner").classList.remove("transform")
+//     // document.getElementById("flip-card-front").classList.remove("transform")
+
+// })
+
+// drawCardBack.addEventListener("click", function() {
+//     flipCardBack()
+// })
 
 function flipCard() {
-    document.getElementById("flip-card").classList.add("transform")
-    document.getElementById("flip-card-inner").classList.add("transform")
+    if (flipCardFront.classList.contains("put-back")) {
+        flipCardFront.classList.remove("put-back")
+    }
+    flipCardInner.classList.add("transform")
+    // let img = document.createElement("img")
+    // img.src = "images/kingofspring.png"
+
+    flipCardBack.style.backgroundImage = "url('images/kingofspring.png')"
+    // document.getElementById("flip-card-front").classList.add("transform")
+    // flipCardFront.classList.add("put-back")
+
 }
 
-function flipCardBack() {
-    document.getElementById("flip-card").classList.remove("transform")
-    document.getElementById("flip-card-inner").classList.remove("transform")
-}
+// function flipCardBack() {
+//     document.getElementById("flip-card").classList.remove("transform")
+//     document.getElementById("flip-card-inner").classList.remove("transform")
+// }
